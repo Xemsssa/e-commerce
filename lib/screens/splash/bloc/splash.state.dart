5d9838fc53e0c1677267771ae303615382d520b4
andrzejwozny <xemsss@gmail.com>
2024-12-19 14:07:31 +0200

    init

---
+++ lib/screens/splash/bloc/splash.state.dart
@@ -0,0 +1,7 @@
+import 'package:e_commerce_app/screens/splash/screen/splash.dart';
+
+abstract class SplashState{}
+
+class DisplaySplash extends SplashState {}
+class Authenticated extends SplashState {}
+class UnAuthenticaded extends SplashState{}
